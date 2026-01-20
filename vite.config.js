fe729448bf34fb3b8476f95c46a3caaf08112afc
N <34268416+ziloen@users.noreply.github.com>
2026-01-20 14:11:06 +0800

    chore: use baseline target

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig(({ command, mode }) => {
   const IS_PROD = !IS_DEV
   const IS_BUILD = command === 'build'
 
-  const target = '> 0.5%, last 2 versions, Firefox ESR, not dead'
+  const target = 'baseline widely available with downstream'
 
   return {
     base: process.env.BASE_URL || '/',
